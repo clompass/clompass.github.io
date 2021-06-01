a4ab8aeea09d6ac3e99e0942aa8ef67e8df635b0
DarccMan <darcy3057@gmail.com>
2021-06-02 09:48:17 +1000

    Client: Added files; Server: Fixed bugs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Clompass
+
+A tool to use Compass Education in a more intuitive way
+
+[Server Side](https://github.com/clompass/server)
--- js/main.js
+++ js/main.js
@@ -1,36 +1,58 @@
-var day;
-var week;
-function init() {
-  d = new Date();
-  day = d.getDay();
-  week = d.getWeek() % 2;
-
-  timetable.init();
+/* Communicate with server */
+var ws = new WebSocket("ws://127.0.0.1:8081");
+ws.onerror = function (err) {
+  console.error(err);
+};
+ws.onmessage = function (event) {
+  event = JSON.parse(event.data);
+  if (event.type == "RETURN") {
+    if (event.name == "timetable") {
+      console.log(event.data);
+      if (event.data) {
+        setTimetable(event.data);
+      }
+    }
+  }
+};
+ws.onclose = function () {
+  console.error("Connection is closed...");
 }
 
-/* Timetable */
-timetable = {};
-timetable.init = function () {
-  index = day + week * 7;
-  codes = user.subjects[index];
-  doc.id("timetable").innerHTML = "";
-  for (i = 0; i < codes.length; i++) {
-    sbj = subjects[codes[i]];
-    time = times.periods[i];
-    time = [parseInt(time.s(0, 2)), time.s(2, -1)];
-    if (time[0] > 13) {
-      time[0] -= 12;
-    }
-    time = time.join(":");
-    room = sbj.room;
-    code = codes[i];
+/* Send login request to server */
+function login() {
+  console.log("Loading...");
+  ws.send(JSON.stringify({
+    type: "GET",
+    name: "timetable",
+    time: Date.now(),
+    username: doc.id("username").value,
+    password: doc.id("password").value,
+  }));
+}
 
+function init() {
+  setTimetable();
+}
+
+/* Format timetable in HTML */
+function setTimetable(timetable) {
+  if (!timetable) {
+    timetable = Array.from({length: 4}, () => { });
+  }
+  doc.id("timetable").innerHTML = "";
+  for (i = 0; i < timetable.length; i++) {
     doc.id("timetable").innerHTML += `
-      <article class="timetable item">
-        <h1>
-          ${time} - ${code} - ${room}
-        </h1>
-      </article>
-    `;
+    <article>
+      <h1>Time: {time}</h1>
+      <h1>Code: {code}</h1>
+      <h1>Room: {room}</h1>
+      <h1>Teacher: {teacher}</h1>
+    </article>
+    `.format({
+      time: "Unknown",
+      code: "Unknown",
+      room: "Unknown",
+      teacher: "Unknown",
+    });
   }
 }
